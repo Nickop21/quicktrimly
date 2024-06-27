bdd532fa172e815f95ab31c5d8a874d36ced0f4f
Nickop21 <ny8244@gmail.com>
2024-06-27 13:34:39 +0530

    landing page addd css clean tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components/ui/button'
-import { createBrowserRouter } from 'react-router-dom'
-import AppLayout from './layouts/AppLayout'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+import LinkPage from "./pages/LinkPage";
+import RedirectLink from "./pages/RedirectLink";
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    element:<AppLayout/>,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-          path:'/',
-          element:""
-      }
-    ]
-  }
-])
+        path: "/",
+        element: <Landing />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/link:id",
+        element: <LinkPage />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
 
 function App() {
- 
-
   return (
-    <>
-      <Button variant="outline">Button</Button>
-
-    </>
-  )
+   <RouterProvider router={router}/>
+  );
 }
 
-export default App
+export default App;
